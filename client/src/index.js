235cefb7d79e108117ec41878fd9ff30cc3bc6c6
Adam Risberg <myniztan@hotmail.com>
2019-08-16 00:10:51 -0500

    Add redux with thunk and logger middleware

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import { store } from "./redux/store";
 
 import Storefront from "./Storefront";
 import Admin from "./Admin";
@@ -10,12 +13,14 @@ import "./index.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/admin" component={Admin} />
-        <Route path="/" component={Storefront} />
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/admin" component={Admin} />
+          <Route path="/" component={Storefront} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
