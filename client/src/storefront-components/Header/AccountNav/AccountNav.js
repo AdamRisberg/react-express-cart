3f9a59acfda0fa60a5c2b509111e50f92bedb1e3
Adam Risberg <myniztan@hotmail.com>
2019-10-08 04:07:24 -0500

    Clean up AccountNav

--- client/src/storefront-components/Header/AccountNav/AccountNav.js
+++ client/src/storefront-components/Header/AccountNav/AccountNav.js
@@ -11,46 +11,19 @@ import styles from "./AccountNav.module.css";
 const AccountNav = props => {
   if (props.loadingUser) return null;
 
-  if (!props.user) {
-    return (
-      <nav className={styles.AccountNav}>
-        <NavItem clickHandler={props.showLogin} name="Sign In" />
-        <NavItem clickHandler={props.showRegister} name="Create Account" />
-        <button
-          className={styles.CartButton}
-          onClick={() => props.history.push("/checkout/cart")}
-        >
-          <CartIcon />
-          <span className={styles.CartLabel}>View Cart (</span>
-          {props.cartSize && props.cartSize}
-          <span className={styles.CartLabel}>)</span>
-        </button>
-      </nav>
-    );
-  }
-
   return (
     <nav className={styles.AccountNav}>
-      {/* <NavItem
-        clickHandler={() => {}}
-        name={`${props.user.firstName} ${props.user.lastName}`}
-        prefix={"/account"}
-        subcategories={[
-          { _id: "0", path: "", name: "Account" },
-          { _id: "1", path: "orders", name: "Orders" },
-          { _id: "2", path: "addresses", name: "Addresses" },
-          {
-            _id: "3",
-            path: "logout",
-            name: "Sign Out",
-            clickHandler: props.logout
-          }
-        ]}
-      /> */}
-      <AccountDropdown
-        text={`${props.user.firstName} ${props.user.lastName}`}
-        logout={props.logout}
-      />
+      {props.user ? (
+        <AccountDropdown
+          text={`${props.user.firstName} ${props.user.lastName}`}
+          logout={props.logout}
+        />
+      ) : (
+        <React.Fragment>
+          <NavItem clickHandler={props.showLogin} name="Sign In" />
+          <NavItem clickHandler={props.showRegister} name="Create Account" />
+        </React.Fragment>
+      )}
       <button
         className={styles.CartButton}
         onClick={() => props.history.push("/checkout/cart")}
